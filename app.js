255afc9b82d91664416bc9c4a9430bb9023d232f
Kevin Lo <kevlo96@gmail.com>
2020-09-21 14:19:22 -0400

    Added html form, JS file, and CS file

---
+++ app.js
@@ -0,0 +1,7 @@
+// base api path https://api.jikan.moe/v3
+// search anime by title https://api.jikan.moe/v3/search/anime?q=${title}
+// search manga by title https://api.jikan.moe/v3/search/manga?q=${title}
+
+let input = document.querySelector("#search")
+let button = document.querySelector("button")
+let userInput = input.value
