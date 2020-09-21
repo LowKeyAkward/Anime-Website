73c5763032d6852744658ef8808f84303c95453f
Kevin Lo <kevlo96@gmail.com>
2020-09-21 15:53:15 -0400

    added a function to search via anime title

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 // base api path https://api.jikan.moe/v3
-// search anime by title https://api.jikan.moe/v3/search/anime?q=${title}
-// search manga by title https://api.jikan.moe/v3/search/manga?q=${title}
+// search anime by title https://api.jikan.moe/v3/search/anime?q=${userInput}
+// search manga by title https://api.jikan.moe/v3/search/manga?q=${userInput}
 
 let input = document.querySelector("#search")
-let button = document.querySelector("button")
+let button = document.querySelector("#button")
 let userInput = input.value
+
+// let test = async () => {
+//   try {
+//     let response = await axios.get(`https://api.jikan.moe/v3/search/anime?q=kimetsu`)
+//     console.log(response.data.results);
+//   } catch (error) {
+//     console.log(`Error: ${error}`);
+//   }
+// }
+
+// test()
+
+let getTitle = async () => {
+  try {
+    let response = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${userInput}`)
+    console.log(response)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
