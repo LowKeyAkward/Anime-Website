430b443db315c21d252785b9f38bdf45ad7c833e
Kevin Lo <kevlo96@gmail.com>
2020-09-22 14:50:29 -0400

    added search result function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 let input = document.querySelector("#search")
 let button = document.querySelector("#button")
-let userInput = input.value
+// let userInput = input.value
 
 // let test = async () => {
 //   try {
@@ -17,11 +17,33 @@ let userInput = input.value
 
 // test()
 
-let getTitle = async () => {
+button.addEventListener("click", async (e) => {
   try {
-    let response = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${userInput}`)
-    console.log(response)
+    e.preventDefault();
+    let userInput = input.value
+    let response = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${userInput}&limit=10`)
+    console.log(response.data.results)
+    // searchResult(response.data.results)
   } catch (error) {
     console.log(`Error: ${error}`)
   }
+})
+
+let resultDisplay = document.querySelector("result")
+
+let searchResult = searches => {
+  searches.forEach(search => {
+    let searchContainer = document.createElement("div")
+    searchContainer.className = "search-result"
+
+    // let image = document.createElement("img")
+    // image.setAttribute("src", search.image_url)
+    // searchContainer.appendChild(image)
+
+    let title = document.createElement("h3")
+    title.innerHTML = `${search.title}`
+    searchContainer.appendChild(title)
+
+  })
+  resultDisplay.appendChild(searchContainer)
 }
